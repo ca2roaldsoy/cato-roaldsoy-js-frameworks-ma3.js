5ee2869ee2c5c0e7ee5b31b36c93292fb755e0f4
ca2roaldsoy <roaldsoyen@gmail.com>
2020-03-10 22:42:36 +0100

    added home content

---
+++ src/components/home/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Heading from '../Heading.js';
+import homeContent from './HomeContent.js';
+
+function Home() {
+    
+    return (
+        <div>
+            <Heading title="Teenage Mutant Ninja Turtles" />
+            <homeContent>
+
+                Teenage Mutant Ninja Turtles<br />
+                Teenage Mutant Ninja Turtles<br />
+                Teenage Mutant Ninja Turtles<br />
+                Heroes in a half-shell<br />
+                Turtle power!<br /><br />
+
+                They're the world's most fearsome fighting team (We're really hip!)<br />
+                They're heroes in a half-shell and they're green (Hey - get a grip!)<br />
+                When the evil Shredder attacks<br />
+                These Turtle boys don't cut him no slack!<br /><br />
+
+                Teenage Mutant Ninja Turtles<br />
+                Teenage Mutant Ninja Turtles<br /><br />
+
+                Splinter taught them to be ninja teens (He's a radical rat!)<br />
+                Leonardo leads, Donatello does machines (That's a fact, Jack!)<br />
+                Raphael is cool but crude (Gimme a break!)<br />
+                Michaelangelo is a party dude (Party!)<br /><br />
+
+                Teenage Mutant Ninja Turtles<br />
+                Teenage Mutant Ninja Turtles<br />
+                Teenage Mutant Ninja Turtles<br />
+                Heroes in a half shell<br />
+                Turtle power!
+
+            </homeContent>
+        </div>
+    )
+}
+
+export default Home;
