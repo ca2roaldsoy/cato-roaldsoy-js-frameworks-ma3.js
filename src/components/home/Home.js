fb7f1e3bc30a970aa34160443cc9d7f500b6c5d4
ca2roaldsoy <roaldsoyen@gmail.com>
2020-03-10 22:57:49 +0100

    changed content

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,33 +6,11 @@ function Home() {
     
     return (
         <div>
-            <Heading title="Teenage Mutant Ninja Turtles" />
+            <Heading title="Pokemon" />
             <homeContent>
 
-                Teenage Mutant Ninja Turtles<br />
-                Teenage Mutant Ninja Turtles<br />
-                Teenage Mutant Ninja Turtles<br />
-                Heroes in a half-shell<br />
-                Turtle power!<br /><br />
-
-                They're the world's most fearsome fighting team (We're really hip!)<br />
-                They're heroes in a half-shell and they're green (Hey - get a grip!)<br />
-                When the evil Shredder attacks<br />
-                These Turtle boys don't cut him no slack!<br /><br />
-
-                Teenage Mutant Ninja Turtles<br />
-                Teenage Mutant Ninja Turtles<br /><br />
-
-                Splinter taught them to be ninja teens (He's a radical rat!)<br />
-                Leonardo leads, Donatello does machines (That's a fact, Jack!)<br />
-                Raphael is cool but crude (Gimme a break!)<br />
-                Michaelangelo is a party dude (Party!)<br /><br />
-
-                Teenage Mutant Ninja Turtles<br />
-                Teenage Mutant Ninja Turtles<br />
-                Teenage Mutant Ninja Turtles<br />
-                Heroes in a half shell<br />
-                Turtle power!
+                Pokemon!<br />
+                Gotta catch em' all!
 
             </homeContent>
         </div>
