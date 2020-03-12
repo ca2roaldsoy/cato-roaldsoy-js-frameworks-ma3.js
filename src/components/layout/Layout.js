114edba2a955dd64095a53115943d1669a786d09
ca2roaldsoy <roaldsoyen@gmail.com>
2020-03-12 18:01:15 +0100

    added react bootstrap, styled navbar

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,6 +2,9 @@ import React from "react";
 import Home from '../home/Home.js';
 import News from '../news/News.js';
 import Login from '../login/Login.js';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from "react-bootstrap/Container";
 import {
     BrowserRouter as Router,
     Switch,
@@ -15,19 +18,26 @@ function Layout () {
     
         <Router>
            
-            <nav>
-               <ul>
-                    <li><NavLink to="/" exact> Home </NavLink></li>
-                    <li><NavLink to="/news" > News </NavLink></li>
-                    <li><NavLink to="/login" > Login </NavLink></li>
-                </ul>
-            </nav>
+            <Navbar bg="dark" variant="dark" expand="lg" role="navigation">
+              <Navbar.Brand href="#home">POKEMASTER</Navbar.Brand>
+               <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                   <Nav as="ul" className="mr-auto">
+                       
+                            <li className="px-2"><NavLink to="/" exact> Home </NavLink></li>
+                            <li className="px-2"><NavLink to="/news" > News </NavLink></li>
+                            <li className="px-2"><NavLink to="/login" > Login </NavLink></li>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
             
-            <Switch>
-               <Route path="/" exact component = { Home } />
-               <Route path="/news" component = { News } />
-               <Route path="/login" component = { Login } />    
-            </Switch>
+            <Container>
+                <Switch>
+                   <Route path="/" exact component = { Home } />
+                   <Route path="/news" component = { News } />
+                   <Route path="/login" component = { Login } />    
+                </Switch>
+            </Container>
             
         </Router>
     )
